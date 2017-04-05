9fae4de65c735b02fe76b27b092d14d13eb89dce
dryna <kubadrynkowski@gmail.com>
2017-04-05 07:45:08 +0200

    handling errors in frontend

--- admin/client/App/screens/Item/actions.js
+++ admin/client/App/screens/Item/actions.js
@@ -136,7 +136,7 @@ export function deleteItem (id, router) {
 			}
 			// TODO Proper error handling
 			if (err) {
-				alert('Error deleting item, please try again!');
+				alert(err.errors);
 			} else {
 				dispatch(loadItems());
 			}
